3386ad9a78047e25cde5329f056edcccaa01cd96
ming.zhang <allen.m.zhang@gmail.com>
2020-11-23 20:24:41 +0900

    First commit

---
+++ server.py
@@ -0,0 +1,43 @@
+import os
+
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('./index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            print(data)
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not save to database'
+    else:
+        return 'somethoing went wrong'
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+app.run()
